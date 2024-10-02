e106b90717ff1810f270e28e7630bd55781b34d3
msruhan <masruhan.khotibb@gmail.com>
2024-10-02 11:37:36 +0700

    Add : bank description in Midtrans

--- application/controllers/api/midtrans.php
+++ application/controllers/api/midtrans.php
@@ -25,27 +25,30 @@ class Midtrans extends FSD_Controller
             if($res['transaction_status'] == 'settlement') {
 
                 $this->db->trans_start();
-
+            
                 $result_sql = $this->db->query("SELECT `value` FROM app_settings WHERE `key` = 'idr'");
                 $idr = $result_sql->row()->value;
-
+            
                 // update data
                 $updt['TransactionDateTime']   = date("Y-m-d H:i:s");
                 $updt['TransactionStatus']     = $res['transaction_status'];
                 $result = $this->db->update('midtrans_transaction', $updt, array('OrderID' => $res['order_id']));
-
+            
                 // insert amount
                 $explode = explode('|', $res['order_id']);
                 $transaction_id = 1 + $this->credit_model->get_max_transaction_id(array('TransactionCode' => CASH_PAYMENT_RECEIVED));
                 
+                // Ambil nilai bank dari va_numbers
+                $bank = isset($res['va_numbers'][0]['bank']) ? strtoupper($res['va_numbers'][0]['bank']) : 'UNKNOWN';
+            
                 $ins['MemberID']          = $explode[1];
                 $ins['TransactionCode']   = CASH_PAYMENT_RECEIVED;
                 $ins['TransactionID']     = $transaction_id;
-                $ins['Description']       = "Added By Midtransx";
+                $ins['Description']       = "Added By Midtrans - Bank: " . $bank; // Deskripsi dengan bank
                 $ins['Amount']            = $res['gross_amount'] / $idr;
                 $ins['CreatedDateTime']   = date("Y-m-d H:i:s");
                 $result_insert = $this->db->insert('gsm_credits', $ins);
-
+            
                 if ($result && $result_insert && $idr) {
                     $this->db->trans_complete();
                     echo json_encode(['message' => 'Payment data updated successfully']);
@@ -53,14 +56,15 @@ class Midtrans extends FSD_Controller
                     $this->db->trans_rollback();
                     echo json_encode(['message' => 'Payment data failed']);
                 }
-            } else {
+            }            
+            else {
 
                 $explode = explode('|', $res['order_id']);
 
                 $ins['MemberID']          = $explode[1];
                 $ins['TransactionCode']   = CASH_PAYMENT_RECEIVED;
                 $ins['OrderID']           = $res['order_id'];
-                $ins['Description']       = "Added By Midtransz";
+                $ins['Description']       = "Added By Midtrans";
                 $ins['Amount']            = $res['gross_amount'];
                 $ins['CreatedDateTime']   = date("Y-m-d H:i:s");
                 $ins['TransactionStatus'] = $res['transaction_status'];
