69d2fb6b5873bde1dc1afd2f1f582a4ea76f4835
msruhan <masruhan.khotibb@gmail.com>
2025-08-30 07:10:55 +0700

    Add catatan_aktivitas model and update fileservices controller to use it for logging

--- application/models/catatan_aktivitas.php
+++ application/models/catatan_aktivitas.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Log_model extends CI_Model {
+class catatan_aktivitas extends CI_Model {
     public function get_logs($limit = 100) {
         $this->db->order_by('created_at', 'DESC');
         $query = $this->db->get('activity_log', $limit);
